8f3f3c26984f3d1f9f96714b9e84f8aa468f3a01
duchunxiu <1541131845@qq.com>
2022-03-22 15:29:12 +0800

    Update java_basic.java

--- java_basic.java
+++ java_basic.java
@@ -1,1 +1,9 @@
-1. 使用Pair, Pairy<>(key, value)用于关联键值对。通过组合
+1. 使用Pair, Pairy<>(key, value)用于关联键值对。通过与组合Set集合。可判断集合中有无(key、value),相比HashMap更简单。
+    Set<Pair<Integer, Integer>> obstacleSet = new HashSet<>();
+    for (int i = 0; i < obstacles.length; i++) {
+        obstacleSet.add(new Pair<>(obstacles[i][0], obstacles[i][1]));
+    }
+
+    if (!obstacleSet.contains(new Pair<>(nx, ny))) {
+           ...
+    }
